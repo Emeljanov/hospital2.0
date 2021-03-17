92d15b30250e5a67a243a162e9d07184b4af6881
Emeljanov <anton.yemialyanau@gmail.com>
2021-03-17 11:36:46 +0300

    add test for auth controller

---
+++ hospital/src/test/java/by/emel/anton/restcontroller/v1/AuthenticationRestControllerTest.java
@@ -0,0 +1,122 @@
+package by.emel.anton.restcontroller.v1;
+
+import by.emel.anton.entity.Role;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Sql(value = {"classpath:before-each.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class AuthenticationRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String REQUEST_BODY_USER_PATH = "src/test/resources/request-body-login-user.json";
+    private final String REQUEST_BODY_WRONG_USER_PATH = "src/test/resources/request-body-login-wrong-user.json";
+    private final String KEY_ID = "id";
+    private final String KEY_USER = "user";
+    private final String KEY_TOKEN = "token";
+    private final String KEY_ROLE_STRING = "roleString";
+    private final String KEY_FIRST_NAME = "firstName";
+    private final String KEY_LAST_NAME = "lastName";
+    private final String KEY_BIRTHDAY = "birthday";
+    private final String KEY_ACTIVE = "active";
+    private final LocalDate adminBirthday = LocalDate.of(1800, 1, 1);
+
+    @Test
+    void shouldAuthenticate() throws Exception {
+
+        String userString = getJsonStringFromFile(REQUEST_BODY_USER_PATH);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/login").content(userString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject userResponseJson = new JSONObject(mvcResult.getResponse().getContentAsString());
+        JSONObject userDetailsResponseJson = userResponseJson.getJSONObject(KEY_USER);
+
+        assertNotNull(userResponseJson.getString(KEY_TOKEN));
+        assertEquals(userDetailsResponseJson.getInt(KEY_ID), 3);
+        assertEquals(userDetailsResponseJson.getString(KEY_ROLE_STRING), Role.ADMIN.toString());
+        assertEquals(userDetailsResponseJson.getString(KEY_FIRST_NAME), "Drakula");
+        assertEquals(userDetailsResponseJson.getString(KEY_LAST_NAME), "Pensilvanskiy");
+        assertEquals(userDetailsResponseJson.getString(KEY_BIRTHDAY), adminBirthday.toString());
+        assertTrue(userDetailsResponseJson.getBoolean(KEY_ACTIVE));
+
+    }
+
+    @Test
+    void shouldGetBadRequestStatusWhenRequestBodyNotExist() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/login"))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+    }
+
+    @Test
+    void shouldGetUnauthorizedStatusWhenUserNotExist() throws Exception {
+
+        String userString = getJsonStringFromFile(REQUEST_BODY_WRONG_USER_PATH);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/login").content(userString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+
+    @Test
+    @WithUserDetails("doctor")
+    void shouldLogout() throws Exception {
+
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/logout"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    @Test
+    void shouldGetForbiddenStatusWhenNoAuth() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/logout"))
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+    }
+
+    private String getJsonStringFromFile(String path) throws IOException {
+        return Files.readString(Paths.get(path));
+    }
+}
--- hospital/src/test/java/by/emel/anton/restcontroller/v1/CardsRestControllerTest.java
+++ hospital/src/test/java/by/emel/anton/restcontroller/v1/CardsRestControllerTest.java
@@ -1,24 +1,282 @@
 package by.emel.anton.restcontroller.v1;
 
+import by.emel.anton.entity.PatientCard;
+import by.emel.anton.repository.jpa.PatientCardJpaRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Sql(value = {"classpath:before-each.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class CardsRestControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private PatientCardJpaRepository patientCardJpaRepository;
+
+    private final String REQUEST_BODY_CARD_PATH = "src/test/resources/request-body-create-card.json";
+    private final String REQUEST_BODY_EXIST_CARD_PATH = "src/test/resources/request-wrong-body-create-card.json";
+
+    private final String KEY_ID = "id";
+    private final String KEY_DESCRIPTION = "description";
+    private final String KEY_START_DATE = "startDate";
+    private final String KEY_END_DATE = "endDate";
+    private final String KEY_PATIENT_ID = "patientId";
+    private final String KEY_CARD_ID = "cardId";
+    private final String KEY_DOCTOR_ID = "doctorId";
+    private LocalDate startDate = LocalDate.of(2021, 1, 1);
+    private LocalDate endDate = LocalDate.of(2025, 1, 1);
+
     @Test
-    void findAllCards() {
+    @WithUserDetails("doctor")
+    void shouldFindAllCards() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+        assertEquals(jsonArray.length(), 2);
+
+        JSONObject cardOne = jsonArray.getJSONObject(0);
+
+        assertEquals(cardOne.getInt(KEY_ID), 1);
+        assertEquals(cardOne.getInt(KEY_PATIENT_ID), 2);
+
+        JSONArray jsonArrayFromCardOne = new JSONArray(cardOne.getString("therapyDTOList"));
+        assertEquals(jsonArrayFromCardOne.length(), 1);
+
+        JSONObject therapyJson = jsonArrayFromCardOne.getJSONObject(0);
+        assertEquals(therapyJson.getInt(KEY_ID), 1);
+        assertEquals(therapyJson.get(KEY_DESCRIPTION), "Some description");
+        assertEquals(therapyJson.get(KEY_START_DATE), startDate.toString());
+        assertEquals(therapyJson.get(KEY_END_DATE), endDate.toString());
+        assertEquals(therapyJson.get(KEY_CARD_ID), 1);
+        assertEquals(therapyJson.get(KEY_DOCTOR_ID), 1);
+
+        JSONObject cardTwo = jsonArray.getJSONObject(1);
+        JSONArray jsonArrayFromCardTwo = new JSONArray(cardTwo.getString("therapyDTOList"));
+        assertEquals(jsonArrayFromCardTwo.length(), 0);
+
     }
 
     @Test
-    void findCardById() {
+    @WithUserDetails("patient")
+    void shouldGetUnauthorizedStatusWhenNoAuthorizationInGet() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards"))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
     }
 
     @Test
-    void findCardByPatientId() {
+    void shouldGetBadRequestStatusWhenNoAuthenticationInGet() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards"))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+
+    @Test
+    @WithUserDetails("doctor")
+    void findCardById() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 1))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject cardOne = new JSONObject(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(cardOne.getInt(KEY_ID), 1);
+        assertEquals(cardOne.getInt(KEY_PATIENT_ID), 2);
+
+        JSONArray jsonArrayFromCardOne = new JSONArray(cardOne.getString("therapyDTOList"));
+        assertEquals(jsonArrayFromCardOne.length(), 1);
+
+        JSONObject therapyJson = jsonArrayFromCardOne.getJSONObject(0);
+        assertEquals(therapyJson.getInt(KEY_ID), 1);
+        assertEquals(therapyJson.get(KEY_DESCRIPTION), "Some description");
+        assertEquals(therapyJson.get(KEY_START_DATE), startDate.toString());
+        assertEquals(therapyJson.get(KEY_END_DATE), endDate.toString());
+        assertEquals(therapyJson.get(KEY_CARD_ID), 1);
+        assertEquals(therapyJson.get(KEY_DOCTOR_ID), 1);
+
     }
 
     @Test
-    void saveCard() {
+    @WithUserDetails("doctor")
+    void shouldGetBadRequestStatusWhenNoAuthorizationInGetById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 0))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("patient")
+    void shouldGetUnauthorizedStatusWhenNoAuthorizationInGetById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 1))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetBadRequestStatusWhenNoAuthenticationInGetById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 1))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+
+    @Test
+    @WithUserDetails("doctor")
+    void findCardByPatientId() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/patients/{id}", 2))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject cardOne = new JSONObject(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(cardOne.getInt(KEY_ID), 1);
+        assertEquals(cardOne.getInt(KEY_PATIENT_ID), 2);
+
+        JSONArray jsonArrayFromCardOne = new JSONArray(cardOne.getString("therapyDTOList"));
+        assertEquals(jsonArrayFromCardOne.length(), 1);
+
+        JSONObject therapyJson = jsonArrayFromCardOne.getJSONObject(0);
+        assertEquals(therapyJson.getInt(KEY_ID), 1);
+        assertEquals(therapyJson.get(KEY_DESCRIPTION), "Some description");
+        assertEquals(therapyJson.get(KEY_START_DATE), startDate.toString());
+        assertEquals(therapyJson.get(KEY_END_DATE), endDate.toString());
+        assertEquals(therapyJson.get(KEY_CARD_ID), 1);
+        assertEquals(therapyJson.get(KEY_DOCTOR_ID), 1);
+    }
+
+    @Test
+    @WithUserDetails("doctor")
+    void shouldGetBadRequestStatusWhenNoAuthorizationInGetByPatientId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 0))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("patient")
+    void shouldGetUnauthorizedStatusWhenNoAuthorizationInGetByPatientId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 2))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetBadRequestStatusWhenNoAuthenticationInGetByPatientId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/cards/{id}", 2))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("doctor")
+    void saveCard() throws Exception {
+
+        String cardString = getJsonStringFromFile(REQUEST_BODY_CARD_PATH);
+        JSONObject cardJsonFromBody = new JSONObject(cardString);
+
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.post("/api/v1/cards").content(cardString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        PatientCard patientCardDB = patientCardJpaRepository.findByPatientId(6).get();
+        assertEquals(cardJsonFromBody.getInt(KEY_PATIENT_ID), patientCardDB.getPatient().getId());
+
+        JSONObject responseJsonCard = new JSONObject(mvcResult.getResponse().getContentAsString());
+        assertEquals(responseJsonCard.getInt(KEY_ID), patientCardDB.getId());
+        assertEquals(responseJsonCard.getInt(KEY_PATIENT_ID), patientCardDB.getPatient().getId());
+
+        JSONArray jsonArrayTherapiesDTO = responseJsonCard.getJSONArray("therapyDTOList");
+        assertEquals(jsonArrayTherapiesDTO.length(), 0);
+
+    }
+
+    @Test
+    @WithUserDetails("doctor")
+    void shouldGetBadRequestStatusWhenNoAuthorizationInPost() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/cards"))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+
+    @Test
+    @WithUserDetails("patient")
+    void shouldGetUnauthorizedStatusWhenNoAuthorizationInPost() throws Exception {
+
+        String cardString = getJsonStringFromFile(REQUEST_BODY_CARD_PATH);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/cards").content(cardString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetBadRequestStatusWhenNoAuthenticationInPost() throws Exception {
+
+        String cardString = getJsonStringFromFile(REQUEST_BODY_CARD_PATH);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/cards").content(cardString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("doctor")
+    void shouldGetBadRequestStatusWhenNoAuthenticationInPostWhenCardExist() throws Exception {
+
+        String cardString = getJsonStringFromFile(REQUEST_BODY_EXIST_CARD_PATH);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/cards").content(cardString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    private String getJsonStringFromFile(String path) throws IOException {
+        return Files.readString(Paths.get(path));
     }
 }
